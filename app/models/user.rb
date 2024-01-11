7b0cab5ea260ff2db16346bb3be789ac32db677d
Jonnyhorse <yamasaki199006@gmail.com>
2024-01-11 16:26:39 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
+  validates :first_name_chinese_character, presence: true
+  validates :last_name_chinese_character, presence: true
+  validates :first_name_katakana, presence: true
+  validates :last_name_katakana, presence: true
+  validates :nick_name, presence: true
+  validates :date_of_birth, presence: true
 end
