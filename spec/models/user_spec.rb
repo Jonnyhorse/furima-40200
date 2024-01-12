339db56d7f9e3f7900454b821ad611085e85fe4a
Jonnyhorse <yamasaki199006@gmail.com>
2024-01-12 16:04:55 +0900

    Factorybotの作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
   describe 'ユーザー新規登録' do
     it 'nick_nameが空では登録できない' do
-      user = User.new(nick_name: '', email: 'test@example', password: '000000', password_confirmation: '000000')
-      user.valid?
-      binding.pry
-      expect(user.errors.full_messages).to include("Nick name can't be blank")
+      @user.nick_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Nick name can't be blank"
     end
     it 'emailが空では登録できない' do
-      # emailが空では登録できないテストコードを記述します
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email can't be blank"
     end
   end
 end
