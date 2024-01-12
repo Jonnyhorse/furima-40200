3fcfefcfb0c9659619befe0361f1ae332f98c93f
Jonnyhorse <yamasaki199006@gmail.com>
2024-01-12 19:48:21 +0900

    ユーザーモデル単体テストコードの記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
     email                 {Faker::Internet.email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    first_name_chinese_character {"山田"}
+    last_name_chinese_character {"孝之"}
+    first_name_katakana {"ヤマダ"}
+    last_name_katakana {"タカユキ"}
+    date_of_birth {19900101}
   end
 end
