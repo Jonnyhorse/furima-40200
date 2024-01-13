8349a8c6a271349bc9af5a758c6b07f29f288cc2
Jonnyhorse <yamasaki199006@gmail.com>
2024-01-13 17:24:05 +0900

    producutモデルとマイグレーションファイルへの記述を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_10_115131) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_13_071037) do
+  create_table "products", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "information", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "delivery_charge_id", null: false
+    t.integer "delivery_area_id", null: false
+    t.integer "delivery_day_id", null: false
+    t.integer "selling_price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
