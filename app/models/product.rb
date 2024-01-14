b9fdb481ca811b703b6c92209a0b63f002cfebd0
Jonnyhorse <yamasaki199006@gmail.com>
2024-01-14 11:19:37 +0900

    バリデーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,10 @@ class Product < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :condition, :delivery_area, :delivery_charge, :delivery_day
+
+  #空の投稿を保存できないようにする
+  validates :category, :condition, :delivery_area, :delivery_charge, :delivery_day, presence: true
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id, :condition_id, :delivery_area_id, :delivery_charge_id, :delivery_day_id, numericality: { other_than: 1, message: "can't be blank"}
 end
