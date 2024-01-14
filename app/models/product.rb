d44023e53d34bf27b469beafc100284f158a3690
Jonnyhorse <yamasaki199006@gmail.com>
2024-01-14 10:52:45 +0900

    アクティブハッシュのモデル作成〜アソシエーションの記述を追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
   belongs_to :user
   has_one :purchase_record
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :delivery_area, :delivery_charge, :delivery_day
 end
