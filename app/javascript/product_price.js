2d84fed6b9f7b6fa65dccdcf0e13e0487f8698bd
Jonnyhorse <yamasaki199006@gmail.com>
2024-01-17 17:27:23 +0900

    販売価格から手数料を引いた金額を非同期で表示されるよう記述

--- app/javascript/product_price.js
+++ app/javascript/product_price.js
@@ -7,4 +7,12 @@ priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
   console.log(inputValue);
 
+  const addTaxDom = document.getElementById("add-tax-price");
+  addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+  console.log(addTaxDom);
+
+  const profitDom = document.getElementById("profit");
+  profitDom.innerHTML = Math.floor(inputValue * 0.9);
+  console.log(profitDom)
+
 })
