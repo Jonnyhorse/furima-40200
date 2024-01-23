d8f615c6613a66c37d2558758edeb60cb0e6a76c
Jonnyhorse <yamasaki199006@gmail.com>
2024-01-23 12:03:14 +0900

    公開鍵を設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,10 @@
 const pay = () => {
-  const form = document.getElementById('button')
+  const payjp = Payjp('pk_test_d722e1b308f7f932edb37b9d')// PAY.JPテスト公開鍵
+  const form = document.getElementById('charge-form')
   form.addEventListener("submit", (e) => {
     console.log("フォーム送信時にイベント発火")
     e.preventDefault();
   });
+};
 
-window.addEventListener("turbo:load", pay);
+window.addEventListener("load", pay);
